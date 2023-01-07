212e92f99f1bff206ee28fde613dda6de337e227
SantanuDas2345 <santanudasricxy123@gmail.com>
2023-01-07 20:37:20 +0530

    adding new project

---
+++ index.js
@@ -0,0 +1,21 @@
+const data = {
+    username: 'john',
+    password: 'doe'
+}
+function getItems() {
+    const getValue = document.getElementById('username');
+    const getPassword = document.getElementById('password');
+
+    if(username.value.trim() == '' || password.value.trim() == '' ){
+        alert('No spaces are allowed');
+        return false;
+    }
+
+    else {
+        true;
+    }
+}
+
+function signUp() {
+    
+}
